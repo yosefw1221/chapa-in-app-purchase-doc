46889a7bc6b4b6800bc5119393edbab33ad66a0c
Yosef <yosefworku1221@gmail.com>
2022-11-21 20:31:26 +0300

    add chapa-doc link on navbar

--- docs/.vuepress/configs/navbar/index.js
+++ docs/.vuepress/configs/navbar/index.js
@@ -1,7 +1,11 @@
 
 export const navbar = [
   {
+    text: 'Chapa Docs',
+    link: 'https://developer.chapa.co/docs/',
+  },
+  {
     text: 'LinkedIn',
-    link: 'https://linkedIn/yosef',
+    link: 'https://www.linkedin.com/in/yosef-worku-304a3b1b3/',
   },
 ]
