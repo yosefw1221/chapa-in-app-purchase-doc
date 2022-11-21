3530c2191e0ff3b50a80e392d44910f596a2f55f
Yosef <yosefworku1221@gmail.com>
2022-11-21 20:29:54 +0300

    adds favicon

--- docs/.vuepress/configs/head.js
+++ docs/.vuepress/configs/head.js
@@ -1,39 +1,38 @@
-
 export const head = [
   [
-    'link',
+    "link",
     {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '16x16',
-      href: `/images/icons/favicon-16x16.png`,
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: `/icons/favicon.png`,
     },
   ],
   [
-    'link',
+    "link",
     {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '32x32',
-      href: `/images/icons/favicon-32x32.png`,
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: `/icons/favicon.png`,
     },
   ],
-  ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
-  ['meta', { name: 'application-name', content: 'VuePress' }],
-  ['meta', { name: 'apple-mobile-web-app-title', content: 'VuePress' }],
-  ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+  ["link", { rel: "manifest", href: "/manifest.webmanifest" }],
+  ["meta", { name: "application-name", content: "Chapa-In-App-Purchase" }],
   [
-    'link',
-    { rel: 'apple-touch-icon', href: `/images/icons/apple-touch-icon.png` },
+    "meta",
+    { name: "apple-mobile-web-app-title", content: "Chapa-In-App-Purchase" },
   ],
+  ["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }],
+  ["link", { rel: "apple-touch-icon", href: `/icons/favicon.png` }],
   [
-    'link',
+    "link",
     {
-      rel: 'mask-icon',
-      href: '/images/icons/safari-pinned-tab.svg',
-      color: '#3eaf7c',
+      rel: "mask-icon",
+      href: "/icons/favicon.png",
+      color: "#3eaf7c",
     },
   ],
-  ['meta', { name: 'msapplication-TileColor', content: '#3eaf7c' }],
-  ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-]
+  ["meta", { name: "msapplication-TileColor", content: "#3eaf7c" }],
+  ["meta", { name: "theme-color", content: "#3eaf7c" }],
+];
